5332ad6f26bb721a23d56d44e817b98bf9957e03
Hakim <190618015+MoHakim-01121@users.noreply.github.com>
2026-02-18 08:37:27 +0700

    Update TEMPLATES configuration to include invoices/templates directory

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'invoices' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
